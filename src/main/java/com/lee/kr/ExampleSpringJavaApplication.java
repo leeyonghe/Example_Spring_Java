e7d69cba4d9c3052c959d9bd9bc0b2738f90a0fa
leeyh <leeyh@isung.com>
2019-11-24 17:55:46 +0900

    init

---
+++ src/main/java/com/lee/kr/ExampleSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lee.kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringJavaApplication.class, args);
+	}
+
+}
